1e21f5f55fa5d9c2621a4f28f912ad21bd5e3bf1
pablo di biasi <licdibiasi@gmail.com>
2022-09-05 22:47:45 -0300

    uniendo las 3 partes del simulador

--- javascript/main.js
+++ javascript/main.js
@@ -1,28 +1,68 @@
+// //ingresar productos en venta.(aca ingresa el adm. los productos para a vender, base de datos seria..).
+
 const ProdEnVenta = []
 let cant = parseInt(prompt('cuantos productos desea ingresar'))
 for (i = 0; i < cant; i++) {
   class producto {
-    constructor(nombre, precio) {
+    constructor(id, nombre, precio) {
+      this.id = id
       this.nombre = nombre
       this.precio = precio
     }
   }
+  let ingresarId = parseInt(prompt('ingrese nuevo num de id de producto'))
   let ingresarNombreProducto = prompt('ingrese producto')
   let ingresarValorProducto = parseFloat(
     prompt('ingrese precio de lista del producto'),
   )
 
-  const producto1 = new producto(ingresarNombreProducto, ingresarValorProducto)
+  const producto1 = new producto(
+    ingresarId,
+    ingresarNombreProducto,
+    ingresarValorProducto,
+  )
 
   ProdEnVenta.push(producto1)
 }
+console.log(ProdEnVenta)
+
+//aca abajo creo el array carrito...
+
+let idProdCompra = parseInt(prompt('id del producto a comprar?'))
+
+let carritoPrueba = ProdEnVenta.filter((el) => el.id === idProdCompra)
+
+let otroObjeto = []
+
+let seguirComprando = 'y'
+while (seguirComprando == 'y') {
+  seguirComprando = prompt('desea agregar algun prod al carrito (y/n)')
+
+  if (seguirComprando == 'y') {
+    idProdCompra = parseInt(prompt('id del producto ?'))
+    otroObjeto = ProdEnVenta.filter((el) => el.id === idProdCompra)
+
+    carritoPrueba.push(otroObjeto)
+  } else {
+    alert('ir al carrito para finalizar compra')
+    seguirComprando = 'f'
+  }
+}
+//esto me crea un array con un objeto y array x cada objeto q sumo....
+
+let carrito = carritoPrueba.flat() //con esto lo transformo en array de objetos...
+
+console.log(carrito)
+
+//func.cuotas-----
 
 let precios = []
-for (i in ProdEnVenta) precios.push(ProdEnVenta[i].precio)
+for (i in carrito) precios.push(carrito[i].precio)
 
+console.log(precios)
 const init = 0
 const sum = precios.reduce((anterior, actual) => anterior + actual, init)
-
+console.log(sum)
 function calculoCuota(valorProducto, cantidadCuotas) {
   if (isNaN(valorProducto) || isNaN(cantidadCuotas)) {
     alert('solo se aceptan numeros.')
